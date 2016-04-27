2bc041b319bbd0c1a5c99c9784eae35c7acc36f9
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2016-04-27 20:54:49 -0300

    Added module "clv_file_cst".

---
+++ clv_file_cst/__openerp__.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+
+{
+    'name': 'File (customizations for File Management Solution)',
+    'summary': 'File Module customizations for File Management Solution.',
+    'version': '2.0',
+    'author': 'Carlos Eduardo Vercelino - CLVsol',
+    'category': 'Generic Modules/Others',
+    'license': 'AGPL-3',
+    'website': 'http://clvsol.com',
+    'depends': [
+        'clv_file',
+    ],
+    'data': [
+        'clv_file_view.xml',
+        'seq/clv_file_seq_view.xml',
+        'seq/clv_file_sequence03.xml',
+        'seq/clv_file_sequence04.xml',
+        'seq/clv_file_sequence06.xml',
+        'seq/clv_file_sequence09.xml',
+        'seq/clv_file_sequence10.xml',
+        'seq/clv_file_category_sequence.xml',
+        'clv_file_sequence03.xml',
+        'clv_file_sequence04.xml',
+        'clv_file_sequence06.xml',
+        'clv_file_sequence09.xml',
+        'clv_file_sequence10.xml',
+        'clv_file_category_sequence.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'init_xml': [],
+    'test': [],
+    'update_xml': [],
+    'installable': True,
+    'application': False,
+    'active': False,
+    'css': [],
+}
